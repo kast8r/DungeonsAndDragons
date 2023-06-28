89d9b5222fd12bafea8a3e3865c0f366f5f8f87a
Kastor <loyisk.yt@gmail.com>
2023-06-28 19:22:37 +0200

    Primer apartado

---
+++ src/Controller/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Controller;
+
+import View.Login;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author kastor
+ */
+public class Main {
+
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame login = new Login();
+        login.setVisible(true);
+    }
+    
+}
